d59721101d71e6f24b70a64bcf65c8477115dc94
Mateusz <s19282@pjwstk.edu.pl>
2021-03-28 18:55:04 +0200

    finished

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Rock paper scissors game
@@ -9,47 +10,82 @@ def updateProbability(counter, probability):
 
 
 def game():
-    answers = ['R', 'P', 'S']
-    moves = {'R': 'P', 'P': 'S', 'S': 'R'}
-    number = {'R': 0, 'P': 1, 'S': 2}
+    print("Rock paper scissors game")
+    print("R - Rock")
+    print("P - Paper")
+    print("S - Scissors")
 
+    answers = ['R', 'P', 'S']
+    responses = {'R': 'P', 'P': 'S', 'S': 'R'}
     probability = [[1 / 3, 1 / 3, 1 / 3],
                    [1 / 3, 1 / 3, 1 / 3],
                    [1 / 3, 1 / 3, 1 / 3]]
-
     counter = [[1, 1, 1],
                [1, 1, 1],
                [1, 1, 1]]
-
     # s| next ->
     # t| RR RP RS
     # a| PR PP PS
     # t| SR SP SS
     # e|
+    computerScoreList = []
+    computerScore = 0
+    totalGamesPlayed = 0
     previousMove = 'R'
-
     opponentsMove = "!END"
 
-    while opponentsMove != "END":
+    # x = np.linspace(0, np.pi * 2, 100)
+    # y = np.sin(x)
+    #
+    # plt.plot(x, y)
+    # plt.grid(True)
+    # plt.xlim(0, np.pi * 2)
+    # plt.xlabel("x")
+    # plt.ylabel("f(x) = sin(x)")
+    # plt.title("Wykres funkcji f(x) = sin(x)")
+    # plt.show()
 
+    while opponentsMove != "END":
+        print("-------------------------")
+        predictMove = ""
         if previousMove == 'R':
-            move = np.random.choice(answers, p=probability[0])
-            print(moves.get(move))
+            predictMove = np.random.choice(answers, p=probability[0])
         elif previousMove == 'P':
-            move = np.random.choice(answers, p=probability[1])
-            print(moves.get(move))
+            predictMove = np.random.choice(answers, p=probability[1])
         elif previousMove == 'S':
-            move = np.random.choice(answers, p=probability[2])
-            print(moves.get(move))
+            predictMove = np.random.choice(answers, p=probability[2])
 
         opponentsMove = input('Enter your input:').upper()
-        counter[number.get(previousMove)][number.get(opponentsMove)] += 1
-        updateProbability(counter, probability)
-        previousMove = opponentsMove
-        print(np.array(counter))
-        print(np.array(probability))
+        if opponentsMove == "END":
+            break
+        elif not answers.__contains__(opponentsMove):
+            print("Invalid value, try again!")
+            continue
+        else:
+            print("Computer: ", responses.get(predictMove), " You: ", opponentsMove)
+            if predictMove == opponentsMove:
+                print("Computer won!")
+                computerScore += 1
+            elif responses.get(predictMove) == opponentsMove:
+                print("Draw!")
+            else:
+                print("You won!")
+                computerScore -= 1
+            counter[answers.index(previousMove)][answers.index(opponentsMove)] += 1
+            updateProbability(counter, probability)
+            previousMove = opponentsMove
+            totalGamesPlayed += 1
+            computerScoreList.append(computerScore)
 
+    print("-------------------------")
     print("Game ended!")
+    print("Computer score: ", computerScore)
+    print("Games played: ", totalGamesPlayed)
+    plt.plot(np.arange(len(computerScoreList)), computerScoreList, 'r*')
+    plt.xlabel("Iteration")
+    plt.ylabel("Value")
+    plt.title("Computer score")
+    plt.show()
 
 
 game()
