4f2c20ac5959f736bd3a1b760a23cb6d80efcad5
Mateusz <s19282@pjwstk.edu.pl>
2021-04-08 19:40:00 +0200

    no warning

--- plotka.py
+++ plotka.py
@@ -18,5 +18,5 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     plt.ylim(xx2.min(), xx2.max())
 
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colorsMap(idx), marker=markers[idx], label=cl,
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, color=colorsMap(idx), marker=markers[idx], label=cl,
                     edgecolor='black')
