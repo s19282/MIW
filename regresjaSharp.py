6a67a0f59feb5dcaa4be05ebd8154174c645312c
Mateusz <s19282@pjwstk.edu.pl>
2021-04-17 20:59:35 +0200

    arctan

--- regresjaSharp.py
+++ regresjaSharp.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-a = np.loadtxt('data/dane6.txt')
+a = np.loadtxt('data/dane2.txt')
 
 x = a[:, [1]]
 y = a[:, [0]]
@@ -10,17 +10,21 @@ y = a[:, [0]]
 c = np.hstack([x, np.ones(x.shape)])  # model 1 liniowy
 # c1 = np.hstack([x ** 2, x, np.ones(x.shape)])  # model kwadratowy
 # c2 = np.hstack([1 / x, np.ones(x.shape)])  # model inny
+c4 = np.hstack([np.arctan(x), np.ones(x.shape)])  # arcTan
+
 
 # print(c1)
 
 v = np.linalg.pinv(c) @ y
 # v1 = np.linalg.pinv(c1) @ y
 # v2 = np.linalg.inv(c2.T @ c2) @ c2.T @ y
+v4 = np.linalg.pinv(c4) @ y
 
 # print(v)
 
 plt.plot(x, y, 'ro')
 plt.plot(x, v[0] * x + v[1])
+plt.plot(x, v4[0] * np.arctan(x) + v4[1])
 # plt.plot(x, v1[0] * x ** 2 + v1[1] * x + v1[2])
 # plt.plot(x, v2[0] / x + v2[1])
 plt.show()
