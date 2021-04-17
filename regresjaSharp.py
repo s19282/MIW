7422884f0ae7dd2becacf7ae0a291b2367f0776e
Mateusz <s19282@pjwstk.edu.pl>
2021-04-17 22:12:10 +0200

    calculated errors

--- regresjaSharp.py
+++ regresjaSharp.py
@@ -7,24 +7,18 @@ a = np.loadtxt('data/dane2.txt')
 x = a[:, [1]]
 y = a[:, [0]]
 
-c = np.hstack([x, np.ones(x.shape)])  # model 1 liniowy
-# c1 = np.hstack([x ** 2, x, np.ones(x.shape)])  # model kwadratowy
-# c2 = np.hstack([1 / x, np.ones(x.shape)])  # model inny
-c4 = np.hstack([np.arctan(x), np.ones(x.shape)])  # arcTan
-
-
-# print(c1)
+c = np.hstack([np.cbrt(x), np.ones(x.shape)])  # cube root
+c1 = np.hstack([np.arctan(x), np.ones(x.shape)])
 
 v = np.linalg.pinv(c) @ y
-# v1 = np.linalg.pinv(c1) @ y
-# v2 = np.linalg.inv(c2.T @ c2) @ c2.T @ y
-v4 = np.linalg.pinv(c4) @ y
+v1 = np.linalg.pinv(c1) @ y
 
-# print(v)
+e = np.average((sum(y - v[0] * np.cbrt(x) + v[1]) ** 2))
+e1 = np.average(sum((y - v1[0] * np.arctan(x) + v1[1]) ** 2))
+print("cube root: ", e)
+print("arcus tangens: ", e1)
 
 plt.plot(x, y, 'ro')
-plt.plot(x, v[0] * x + v[1])
-plt.plot(x, v4[0] * np.arctan(x) + v4[1])
-# plt.plot(x, v1[0] * x ** 2 + v1[1] * x + v1[2])
-# plt.plot(x, v2[0] / x + v2[1])
+plt.plot(x, v[0] * np.cbrt(x) + v[1])
+plt.plot(x, v1[0] * np.arctan(x) + v1[1])
 plt.show()
