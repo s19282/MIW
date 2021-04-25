5f6c7b9102cb2c19a9e129550815cf02f9fd26bd
Mateusz <s19282@pjwstk.edu.pl>
2021-04-25 22:57:44 +0200

    added relu, not work yet

--- Data/RELU.py
+++ Data/RELU.py
@@ -11,10 +11,10 @@ from sklearn.model_selection import train_test_split
 #
 # xTraining, xTest, yTraining, yTest = train_test_split(x, y, test_size=0.7)
 
-p = np.hstack(-2, 0.1, 2)
+p = np.hstack(-4, 0.1, 4)
 t = p ** 2 + 1*(np.random.rand(p) - .5)
 
-s1 = 100
+s1 = 2
 w1 = random.randint(s1, 1) - .5
 b1 = random.randint(s1, 1) - .5
 w2 = random.randint(1, s1) - .5
@@ -22,8 +22,9 @@ b2 = random.randint(1, 1) - .5
 
 lr = 0.001
 
-for x in range(1, 20):
-    a1 = np.tanh(w1 * p, + b1 * np.ones(p))
+for epoch in range(1, 200):
+    x = w1 * p + b1 * np.ones(p)
+    a1 = max(x, 0)
     a2 = w2 * a1 + b2
 
     e2 = t - a2
@@ -31,10 +32,15 @@ for x in range(1, 20):
 
     dw2 = lr * e2 * a1.T
     db2 = lr * e2 * np.ones(e2).T
-    dw1 = lr * (1 - a1 * a1) * e1 * p.T
-    db1 = lr * (1 - a1 * a1) * e1 * np.ones(p).T
+    dw1 = lr * (np.exp(x)/(np.exp(x)+1)) * e1 * p.T
+    db1 = lr * (np.exp(x)/(np.exp(x)+1)) * e1 * np.ones.T
 
-    if x % 1 == 0:
+    w2 = w2 + dw2
+    b2 = b2 + db2
+    w1 = w1 + dw1
+    b1 = b1 + db1
+
+    if epoch % 10 == 0:
         plt.clf()
         plt.plot(p, t, 'r*')
         plt.plot(p, a2)
