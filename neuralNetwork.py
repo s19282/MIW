0fc14d4e38002b466eaf39183a4cf38eb72a3fb4
Mateusz <s19282@pjwstk.edu.pl>
2021-04-26 11:42:07 +0200

    removed relu, changed * with @, not work yet

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,38 +1,29 @@
-from random import random
 from time import sleep
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 
-# a = np.loadtxt('data/dane2.txt')
-#
-# x = a[:, [0]]
-# y = a[:, [1]]
-#
-# xTraining, xTest, yTraining, yTest = train_test_split(x, y, test_size=0.7)
-
-p = np.hstack(-2, 0.1, 2)
-t = p ** 2 + 1*(np.random.rand(p) - .5)
-
+p = np.arange(-2, 2, 0.1)
+t = p.T ** 2 + 1*(np.random.rand(p)-0.5)
+# TODO: check t once more
 s1 = 100
-w1 = random.randint(s1, 1) - .5
-b1 = random.randint(s1, 1) - .5
-w2 = random.randint(1, s1) - .5
-b2 = random.randint(1, 1) - .5
+w1 = np.random.rand(s1, 1) - .5
+b1 = np.random.rand(s1, 1) - .5
+w2 = np.random.rand(1, s1) - .5
+b2 = np.random.rand(1, 1) - .5
 
 lr = 0.001
 
 for x in range(1, 20):
-    a1 = np.tanh(w1 * p, + b1 * np.ones(p))
-    a2 = w2 * a1 + b2
+    a1 = np.tanh(w1 @ p + b1 @ np.ones(p))
+    a2 = w2 @ a1 + b2
 
     e2 = t - a2
-    e1 = w2.T * e2
+    e1 = w2.T @ e2
 
-    dw2 = lr * e2 * a1.T
-    db2 = lr * e2 * np.ones(e2).T
-    dw1 = lr * (1 - a1 * a1) * e1 * p.T
-    db1 = lr * (1 - a1 * a1) * e1 * np.ones(p).T
+    dw2 = lr @ e2 @ a1.T
+    db2 = lr @ e2 @ np.ones(e2).T
+    dw1 = lr @ (1 - a1 * a1) * e1 @ p.T
+    db1 = lr @ (1 - a1 * a1) * e1 @ np.ones(p).T
 
     if x % 1 == 0:
         plt.clf()
