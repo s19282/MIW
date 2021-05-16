0c1894fb1de2725ca91d0e1a40f92f074e11e508
Mateusz <s19282@pjwstk.edu.pl>
2021-05-17 01:19:20 +0200

    added more models

--- s1.py
+++ s1.py
@@ -3,18 +3,6 @@ from keras import models
 from keras.datasets import cifar10
 from keras.utils import to_categorical
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-model.summary()
-
-
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 train_images = train_images.reshape((50000, 32, 32, 3))
 train_images = train_images.astype('float32') / 255
@@ -22,8 +10,55 @@ test_images = test_images.reshape((10000, 32, 32, 3))
 test_images = test_images.astype('float32') / 255
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=5, batch_size=64)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+# 3 layers
+model1 = models.Sequential()
+model1.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model1.add(layers.MaxPooling2D((2, 2)))
+model1.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model1.add(layers.Flatten())
+model1.add(layers.Dense(64, activation='relu'))
+model1.add(layers.Dense(10, activation='softmax'))
+model1.summary()
+
+model1.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model1.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+test_loss, test_acc = model1.evaluate(test_images, test_labels)
 print('test_acc = ', test_acc)
+
+# 2 layers
+
+model2 = models.Sequential()
+model2.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model2.add(layers.MaxPooling2D((2, 2)))
+model2.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model2.add(layers.Flatten())
+model2.add(layers.Dense(64, activation='relu'))
+model2.add(layers.Dense(10, activation='softmax'))
+model2.summary()
+
+model2.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model2.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+test_loss, test_acc = model2.evaluate(test_images, test_labels)
+print('test_acc = ', test_acc)
+
+
+# 1 layers
+model3 = models.Sequential()
+model3.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model3.add(layers.MaxPooling2D((2, 2)))
+model3.add(layers.Flatten())
+model3.add(layers.Dense(64, activation='relu'))
+model3.add(layers.Dense(10, activation='softmax'))
+model3.summary()
+
+model3.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model3.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+test_loss, test_acc = model3.evaluate(test_images, test_labels)
+print('test_acc = ', test_acc)
+
