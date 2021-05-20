1938b5173de09e7c7f00c191b717591583864c88
Mateusz <s19282@pjwstk.edu.pl>
2021-05-20 13:16:08 +0200

    divided set into test and training subsets, calculated errors

--- arpred.py
+++ arpred.py
@@ -1,14 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
 a = np.loadtxt('alior.txt')
 
-y = a[:30,[0]]
-c = a[:30,1:]
-v = np.linalg.pinv(c) @ y
+y = a[:50, [0]]
+c = a[:50, 1:]
+cTraining, cTest, yTraining, yTest = train_test_split(c, y, test_size=0.3)
+
+v = np.linalg.pinv(cTraining) @ yTraining
 print(v)
 
-plt.plot(y,'r-')
-plt.plot(v[0]*c[:,[0]] + v[1]*c[:,[1]] + v[2]*c[:,[2]])
-plt.show()
+eTraining = sum((yTraining - (v[0] * cTraining[:, [0]] + v[1] * cTraining[:, [1]] + v[2] * cTraining[:, [2]])) ** 2) / cTraining.shape[0]
+eTest = sum((yTest - (v[0] * cTest[:, [0]] + v[1] * cTest[:, [1]] + v[2] * cTest[:, [2]])) ** 2) / cTest.shape[0]
 
+print("Error Training: ", eTraining)
+print("Error Test: ", eTest)
+
+plt.plot(yTest, 'r-')
+plt.plot(v[0] * cTest[:, [0]] + v[1] * cTest[:, [1]] + v[2] * cTest[:, [2]])
+plt.show()
